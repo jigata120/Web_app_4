d97074bc3c1716e9550813f502c8a98d18fb873b
Mihael Jigov <pplesho@gmail.com>
2023-10-25 20:35:20 +0300

    logout url

--- SMA/Users/urls.py
+++ SMA/Users/urls.py
@@ -3,7 +3,10 @@ from django.urls import path, include
 from Users.views import *
 
 urlpatterns = [
-    path('login/', login, name='login_page'),
-    path('register/', register, name='register_page'),
+    path('login/', login_page, name='login_page'),
+    path('register/', register_page, name='register_page'),
+    path('logout/', logout_page, name='logout_page'),
+
+
 
 ]
