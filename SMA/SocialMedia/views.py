49ff30ae997d34697679deb3afe60c15921b2cc5
Mihael Jigov <pplesho@gmail.com>
2023-10-29 21:10:03 +0200

    @login_required validation

--- SMA/SocialMedia/views.py
+++ SMA/SocialMedia/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.views import generic as views
 from django.contrib.auth.models import User
 
 # Create your views here.
-
+@login_required(login_url = 'login_page')
 class HomeView(views.ListView):
     template_name = 'User_views/index.html'
     model = User
