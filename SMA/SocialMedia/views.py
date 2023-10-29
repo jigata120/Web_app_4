11f2f74925bcf6885930f93754b81296d9e200b7
Mihael Jigov <pplesho@gmail.com>
2023-10-29 21:08:20 +0200

    pagination test

--- SMA/SocialMedia/views.py
+++ SMA/SocialMedia/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
-
+from django.views import generic as views
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+class HomeView(views.ListView):
+    template_name = 'User_views/index.html'
+    model = User
+    context_object_name = 'Users'
+    paginate_by = 3
+
+''''
+page_obj
+paginator
+is_paginated
+object_list
+
 def Index(request):
     context = {
 
     }
     return render(request, 'User_views/Index.html', context)
+'''
