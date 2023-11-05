403202fbc298de1d62979465a65cec238e9dcf25
Mihael Jigov <pplesho@gmail.com>
2023-11-05 22:01:00 +0200

    post model and comment model forms

--- SMA/Users/forms.py
+++ SMA/Users/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
