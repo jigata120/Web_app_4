fa7c3cf6b1fb601e936c85ebec236c7940f2bb5c
Mihael Jigov <pplesho@gmail.com>
2023-11-13 17:23:42 +0200

    urls

--- SMA/SocialMedia/urls.py
+++ SMA/SocialMedia/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 
 
 urlpatterns = [
-     path('',your_view, name='home_page'),
+     path('',HomeView.as_view(), name='home_page'),
     path('add_friends', AddFriendsView.as_view(), name='add_friends_page'),
     path('profile', ProfileView.as_view(), name='profile_page'),
-
+    path('post/<int:pk>/', Post_detail_view, name='post_detail_page'),
+    path('create/post/', Create_post_view, name='create_post_page'),
 ]
