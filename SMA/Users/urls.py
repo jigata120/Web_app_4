79b34959e5da80a3d201e287d68461d182a22b3d
Mihael Jigov <pplesho@gmail.com>
2023-11-27 20:22:24 +0200

    structure

--- SMA/Users/urls.py
+++ SMA/Users/urls.py
@@ -4,11 +4,10 @@ from Users.views import *
 from .views import RegisterView
 from .views import LoginView
 from .views import LogoutView
+
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login_page'),
-    path('register/', RegisterView.as_view() , name='register_page'),
+    path('register/', RegisterView.as_view(), name='register_page'),
     path('logout/', LogoutView.as_view(), name='logout_page'),
-
-
 
 ]
