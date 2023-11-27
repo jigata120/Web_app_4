4003516e81850c055b8ddd4b435a6b1a2ab0b087
Mihael Jigov <pplesho@gmail.com>
2023-11-27 20:22:45 +0200

    structure

--- SMA/Users/forms.py
+++ SMA/Users/forms.py
@@ -3,7 +3,8 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username','email','password1','password2']
+        fields = ['username', 'email', 'password1', 'password2']
