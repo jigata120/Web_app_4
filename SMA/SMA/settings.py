d4f0526ff1306e9e97f0972d756088f7fd7d8027
Mihael Jigov <pplesho@gmail.com>
2023-11-28 17:59:57 +0200

    static

--- SMA/SMA/settings.py
+++ SMA/SMA/settings.py
@@ -132,7 +132,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS =(
     os.path.join(BASE_DIR, 'static'),
 )
-
+STATIC_ROOT=os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
