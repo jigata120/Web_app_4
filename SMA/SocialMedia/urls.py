96f9ec986c2f726c42642d3efc3d02abd5693cf9
mihaeldzhigov <pplesho@gmail.com>
2024-02-27 19:16:25 +0200

    new main page url configs

--- SMA/SocialMedia/urls.py
+++ SMA/SocialMedia/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-     path('',HomeView.as_view(), name='home_page'),
+ path('main', MainView.as_view(), name='main_page'),
     path('add_friends', AddFriendsView.as_view(), name='add_friends_page'),
     path('profile', ProfileView.as_view(), name='profile_page'),
     path('post/<int:pk>/', Post_detail_view, name='post_detail_page'),
