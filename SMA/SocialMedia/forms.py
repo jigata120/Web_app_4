2c8927344b30d8008bb2e879cd2cc9d45ebdd0e1
mihaeldzhigov <pplesho@gmail.com>
2024-03-17 23:14:09 +0200

    comms

--- SMA/SocialMedia/forms.py
+++ SMA/SocialMedia/forms.py
@@ -2,11 +2,11 @@
 from django.forms import ModelForm
 from .models import *
 
-class CreateCommentForm(ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['text']
-class CreatePostForm(ModelForm):
-    class Meta:
-        model = Post
-        fields = ['text', 'image']
+# class CreateCommentForm(ModelForm):
+#     class Meta:
+#         model = Comment
+#         fields = ['text']
+# class CreatePostForm(ModelForm):
+#     class Meta:
+#         model = Post
+#         fields = ['text', 'image']
