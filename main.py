3a6c772ead725c4c2fcef697bb44a172f37fa5e1
mihaeldzhigov <pplesho@gmail.com>
2024-03-21 22:20:52 +0200

    youtube api results

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+from googleapiclient.discovery import build
+from key import API_KEY
+
+# Replace 'YOUR_API_KEY' with your actual API key
+youtube = build('youtube', 'v3', developerKey=API_KEY)
+
+# This example searches for 'Python Tutorial' videos.
+request = youtube.search().list(
+    q='Python Tutorial',
+    part='snippet',
+    type='video',
+    maxResults=10
+)
+
+response = request.execute()
+
+print(response)
 
 
