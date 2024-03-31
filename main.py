7b64f816aaf9ef2995fcc5e40b6ec373f8cfa09a
mihaeldzhigov <pplesho@gmail.com>
2024-03-31 18:45:57 +0300

    api to google drive

--- main.py
+++ main.py
@@ -16,4 +16,19 @@ response = request.execute()
 
 print(response)
 
+from googleapiclient.discovery import build
+from key import API_KEY
+
+drive = build('drive', 'v3', developerKey=API_KEY)
+
+# List files in Google Drive
+results = drive.files().list(pageSize=10).execute()
+items = results.get('files', [])
+
+if not items:
+    print('No files found.')
+else:
+    print('Files:')
+    for item in items:
+        print(u'{0} ({1})'.format(item['name'], item['id']))
 
